cea0d71338eff437ed974ded889bc4c3ad8c3779
leon_2192 <leon.strokes@gmail.com>
2023-11-06 12:21:49 -0300

    feat ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    script: 'npm start',
+  },],
+
+  deploy: {
+    production: {
+      key: 'developer-portfolio.pem',
+      user: 'ubuntu',
+      host: '54.162.60.27',
+      ref: 'origin/main',
+      repo: 'https://github.com/Leon2192/portfolio-deployment.git',
+      path: '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
